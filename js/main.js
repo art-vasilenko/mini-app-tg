fa3dff67b8fb62319a4457c5750301521a0c572a
Artem <vasilenko@maxis-it.ru>
2026-02-27 17:31:24 +0300

    Receiving user from tg

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const tg = window.Telegram.WebApp;
 tg.ready();
 
+const user = tg.initDataUnsafe.user;
+
+if (user) {
+  console.log("User ID:", user.id);
+  console.log("Username:", user.username);
+  console.log("First name:", user.first_name);
+}
+
 const analyzeButton = document.getElementById("analyzeButton");
 const form = document.getElementById("analysisForm");
 
